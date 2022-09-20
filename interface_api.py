102e37dabcc421b436e1b753e382a7a1f3ac449b
patrikprovaznik <patrik.provaznik@pantheon.tech>
2022-09-20 11:21:00 +0200

    Some final changes have been made to the code for example: 1. adding break to nested loops in post_several_interfaces function to lower the amount of cycling 2. removing condition - if not in result because adding break in this function solve this problem too + we also added get() for input_interface 3. adding message with name of deleted/not deleted interface in delete_interface function 4. next we made small changes such as: putting .gitignore into root director and adding recommended type hints

--- interface_api.py
+++ interface_api.py
@@ -1,5 +1,6 @@
 import http
 import json
+from typing import Union, Tuple
 
 from flask import Flask, jsonify, request, Response
 
@@ -25,12 +26,12 @@ def get_all_interfaces() -> Response:
 
 
 @app.route('/get-interface/<path:interface_name>', methods=['GET'])
-def get_some_interface(interface_name: str) -> Response | tuple[str, int]:
+def get_some_interface(interface_name: str) -> Union[Response, Tuple[str, int]]:
     interface = app.cache.get(interface_name)
     if interface:
         return jsonify(interface)
     else:
-        return '', http.HTTPStatus.NOT_FOUND
+        return f'Interface {interface_name} does not exist.', http.HTTPStatus.NOT_FOUND
 
 
 @app.route('/get-interfaces', methods=['POST'])
@@ -43,21 +44,21 @@ def post_several_interfaces() -> Response:
             interface_name = input_interface.get("name")
             interface_type = input_interface.get("type")
             interface_enabled = input_interface.get("enabled")
-            if (interface_name is None or interface["name"] == interface_name) and \
-                    (interface_type is None or interface["type"] == interface_type) and \
-                    (interface_enabled is None or interface["enabled"] == interface_enabled):
-                if interface not in result:
-                    result.append(interface)
+            if (interface_name is None or interface.get("name") == interface_name) and \
+                    (interface_type is None or interface.get("type") == interface_type) and \
+                    (interface_enabled is None or interface.get("enabled") == interface_enabled):
+                result.append(interface)
+                break
     return jsonify(result)
 
 
 @app.route('/delete-interface/<path:interface_name>', methods=['DELETE'])
-def delete_interface(interface_name: str) -> tuple[str, int]:
+def delete_interface(interface_name: str) -> Union[Response, Tuple[str, int]]:
     interface_deleted = app.cache.pop(interface_name, None)
     if interface_deleted:
-        return 'DELETE was Successful', http.HTTPStatus.OK
+        return f'DELETE WAS SUCCESSFUL.\nThe name of the deleted interface is: {interface_name}', http.HTTPStatus.OK
     else:
-        return 'DELETE was Unsuccessful.', http.HTTPStatus.NOT_FOUND
+        return f'DELETE WAS UNSUCCESSFUL.\nInterface {interface_name} does not exist.', http.HTTPStatus.NOT_FOUND
 
 
 if __name__ == '__main__':
